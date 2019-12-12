7696ece597ee716d631f2be4c3ab392e859de417
yokra <yordanka_krasteva@mail.bg>
2019-12-12 22:41:31 +0100

    styling the modal

--- website-page/js/script.js
+++ website-page/js/script.js
@@ -6,3 +6,29 @@ var swiper = new Swiper(".swiper-container", {
     prevEl: ".swiper-button-prev"
   }
 });
+
+// Get the modal
+let modal = document.getElementById("mapModal");
+
+// Get the img that opens the modal
+let btn = document.querySelectorAll("#one, #five, #ten");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
