8bbfddf3a4d9db2ea56089a42acf60365089ebfb
Juliana Mota <julianamota89@gmail.com>
2019-12-14 16:43:09 +0100

    add search list

--- app/js/search.js
+++ app/js/search.js
@@ -27,4 +27,23 @@ function get() {
     });
 }
 
-function displayVolunteers(volunteer) {}
+function displayVolunteers(volunteer) {
+  const clone = document.querySelector("template").content.cloneNode(true);
+  // console.log(clone);
+
+  const dataFields = clone.querySelectorAll("[data-field]");
+  console.log(volunteer);
+
+  dataFields.forEach(element => {
+    const property = element.dataset.field;
+    if (element.dataset.field === "User Image") {
+      element.src =
+        "url(https://anime-8835.restdb.io/media/" +
+        volunteer[property] +
+        "?key=5c7ef096cac6621685acbbb6)";
+    } else {
+      element.textContent = volunteer[property];
+    }
+  });
+  document.querySelector("[data-list=volunteers]").appendChild(clone);
+}
